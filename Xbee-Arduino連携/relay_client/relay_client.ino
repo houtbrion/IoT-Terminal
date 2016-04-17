09c55c079e1098e8b862016ab9bf99e4265d2b91
houtbrion <houtbrion@gmail.com>
2016-04-17 22:18:51 +0900

    ソースを整理したものと入れ替え

--- Xbee-Arduino連携/relay_client/relay_client.ino
+++ Xbee-Arduino連携/relay_client/relay_client.ino
@@ -1,40 +1,62 @@
+/*******************************************************************************
+*  電源が入ると，センサ処理を行い，処理結果をシリアルで外部に送信する
+*  プログラム
+*******************************************************************************/
 
-#define DEBUG
+/*******************************************************************************
+*  機能のON/OFFなど
+*******************************************************************************/
+#define DEBUG                // デバッグメッセージをコンソールに出力するか否か
+#define STARTUP_LED          // 起動時にLEDを明滅させるか否か
+#define SERIAL_OUT           // 処理結果をシリアルでリレーサーバに出力するか否か
+//#define SHUTDOWN             // シリアルサーバに処理終了の信号を出すか否か(シリアル出力しない場合)
+#define USE_RTC_8564NB       // RTCで現在時刻を測定するか否かと利用するRTCの種類の指定
+#define USE_SD              // 処理結果をSDに保存するか否か
 
-#define STARTUP_LED
-#define SERIAL_OUT
-//#define SHUTDOWN
-#define USE_RTC_8564NB
-//#define USE_SD
+/*******************************************************************************
+*  ピン番号の指定
+*******************************************************************************/
+/* 接続相手(リレーサーバ or Xbee)との接続端子の指定 */
+#define SHUTDOWN_PIN 9     // 処理終了をピンから信号を出して通知する場合のピン番号
+#define SERIAL_COM_RX 6    // 処理結果を通知するためのシリアルのRx端子の指定
+#define SERIAL_COM_TX 7    // 処理結果を通知するためのシリアルのTx端子の指定
 
+/* シールドで利用する端子の指定 */
+#define SD_CHIP_SELECT 8   // マイクロSDシールドのチップセレクト(SPI)は8番ピン
+#define RTC_LED_PIN 3          // 処理開始時にLEDを明滅させる場合のLEDのピン番号
+#define SD_LED_PIN 5
+
+/* 使わなくても，定義しておかないとRTCをセットアップできないのでやむを得ず定義しておく */
+#define RTC_PIN_NUMBER 2   // RTCから割り込みを受ける場合のピン番号の指定
+#define INT_NUMBER 0       // RTCの割り込み番号の指定
+
+/* 温度・湿度センサの接続先ピン(センサ処理の例のため) */
 #define DHT_PIN 4            // DHTシリーズの温湿度センサを接続するピン番号
+/*******************************************************************************
+*  定数の指定
+*******************************************************************************/
+#define MAX_SLEEP 100000            // 処理終了後に一定時間シリアル回線を止めないと，なにが起こるかわからないため，長時間眠らせるための指定
+#define LOG_FILE_NAME "log.txt"     // SDカードに処理結果を保存する場合のログファイルの名前
+#define TIME_BUFF_MAX 256           // 時刻情報を文字列に変換するためのバッファサイズ
+#define BUFF_MAX 512                // 処理結果に対する文字列処理を行うためのバッファのサイズ
 
-#define AVR
-
-#define MAX_SLEEP 10000
-#define BUFF_MAX 64
-#define TIME_BUFF_MAX 32
-
-//char buff[BUFF_MAX];
-char sensorVal[BUFF_MAX];
-char tbuff[TIME_BUFF_MAX];
-/**********************************
- * ここから，センサ関係処理の定義
- *  注意事項 :
- *    ・本来この部分はユーザが開発する
- *    ・DHTシリーズのセンサを用いるこのプログラムはあくまでサンプル
- *    ・今回使うのはDHTシリーズセンサ用ライブラリのうち，https://github.com/markruys/arduino-DHT
- **********************************/
-
+/*******************************************************************************
+*  センサ関係処理
+*  注意事項 :
+*    ・本来この部分はユーザが開発する
+*    ・DHTシリーズのセンサを用いるこのプログラムはあくまでサンプル
+*    ・今回使うのはDHTシリーズセンサ用ライブラリのうち，https://github.com/markruys/arduino-DHT
+*******************************************************************************/
+/* 定数やオブジェクトの定義 */
 #define DHT_SENSOR  // 温度湿度センサを用いるか否かの指定
-
-#include <math.h>
+#define MAX_RETRY 5 // センサからデータが正常に読めなかった場合に，リトライする回数の指定
 
 #ifdef DHT_SENSOR
 #include "DHT.h"
 DHT dht;
 #endif /* DHT_SENSOR */
 
+/* センサの起動時の初期化処理 */
 void setupSensor()
 {
 #ifdef DHT_SENSOR
@@ -42,15 +64,14 @@ void setupSensor()
 #endif /* DHT_SENSOR */
 }
 
-#define MAX_RETRY 5 // センサからデータが正常に読めなかった場合に，リトライする回数の指定
-
-void getSensorValue(char * buff) {
+/* センサ処理 */
+void getSensorValue(char * buff){
 #ifdef DHT_SENSOR
   float temperatureVal, humidityVal;
 #ifdef AVR
-  //char temperature[10],humidity[10];
+  char temperature[10],humidity[10];
 #endif
-  for (int i = 0; i < MAX_RETRY ; i++ ) { // 一回ではデータが読み取れないかもしれないため，数回トライする
+  for (int i=0; i < MAX_RETRY ; i++ ){  // 一回ではデータが読み取れないかもしれないため，数回トライする
     delay(dht.getMinimumSamplingPeriod()); // DHTシリーズセンサのデータ読み取り準備のために一定時間待機する
     temperatureVal = dht.getTemperature();
     humidityVal = dht.getHumidity();
@@ -59,49 +80,34 @@ void getSensorValue(char * buff) {
     }
   }
 #ifdef AVR
-  //dtostrf(humidityVal,-1,1,humidity);
-  //dtostrf(temperatureVal,-1,1,temperature);
-  //sprintf(buff,"H = %s , T(C) = %s",humidity,temperature);
-  sprintf(buff, "H = %d , T(C) = %d", round(humidityVal), round(temperatureVal));
+  dtostrf(humidityVal,-1,1,humidity);
+  dtostrf(temperatureVal,-1,1,temperature);
+  sprintf(buff,"Humidity = %s , Temperature(C) = %s",humidity,temperature);
 #endif /* AVR */
 #ifdef GR_KURUMI
-  sprintf(buff, "H= %f , T(C) = %f", humidityVal, temperatureVal);
+  sprintf(buff,"Humidity = %f , Temperature(C) = %f",humidityVal,temperatureVal);
 #endif /* GR_KURUMI */
 #else
-  sprintf(buff, "Hello World!");
+  sprintf(buff,"Hello World!");
 #endif /* DHT_SENSOR */
-#ifdef DEBUG
-  //Serial.println(buff);
-#endif /* DEBUG */
 }
 
 
-/**********************************
- *
- * ここまで
- *
- **********************************/
-
-
-
-
-#ifdef SHUTDOWN
-#define SHUTDOWN_PIN 10
-#endif /* SHUTDOWN */
-
+/*******************************************************************************
+*  ここからプログラム本体
+*******************************************************************************/
+/*******************************************************************************
+*  各種定義
+*******************************************************************************/
 #ifdef SERIAL_OUT
 #include <SoftwareSerial.h>
-#define SERIAL_COM_RX 6
-#define SERIAL_COM_TX 7
 #endif /* SERIAL_OUT */
 
-#ifdef STARTUP_LED
-#define INTERVAL 1000
-#define SHORT_INTERVAL 300
-//const int ledPin =  6;      // the number of the LED pin
-const int ledPin =  13;      // the number of the LED pin
-#endif /* STARTUP_LED */
-
+//#ifdef STARTUP_LED
+//#define INTERVAL 1000
+//#define SHORT_INTERVAL 300
+//const int ledPin =  LED_PIN;      // the number of the LED pin
+//#endif /* STARTUP_LED */
 
 #ifdef SERIAL_OUT
 SoftwareSerial serialCom(SERIAL_COM_RX, SERIAL_COM_TX); // RX, TX
@@ -111,16 +117,12 @@ SoftwareSerial serialCom(SERIAL_COM_RX, SERIAL_COM_TX); // RX, TX
 #include <Wire.h>
 #include <skRTClib.h>
 skRTClib skRTC = skRTClib() ;             // objectの作成
-#define INT_NUMBER 0  // 使わなくても，定義しておかないとRTCをセットアップできないのでやむを得ず
-#define PIN_NUMBER 2
 #endif /* USE_RTC_8564NB */
 
 #ifdef USE_SD
 // SDシールド関係の定義
 #include <SPI.h>
 #include <SdFat.h>
-#define SD_CHIP_SELECT 8 // sparcfunのマイクロSDシールドは8番ピン
-#define LOG_FILE_NAME "log.txt"
 SdFat sd;      // File system object
 SdFile file;  // Log file.
 #define error(msg) sd.errorHalt(F(msg))  // エラーをSDに残すためのマクロ
@@ -128,11 +130,17 @@ SdFile file;  // Log file.
 //#define SDCARD_SPEED SPI_FULL_SPEED  // 性能重視の場合
 #endif /* USE_SD */
 
-// variables will change:
+/*******************************************************************************
+*  初期化処理
+*******************************************************************************/
 void setup() {
+#ifdef STARTUP_LED
+  // initialize the LED pin as an output:
+  pinMode(RTC_LED_PIN, OUTPUT);
+  pinMode(SD_LED_PIN, OUTPUT);
+#endif /*  STARTUP_LED */
   setupSensor();
-
-#ifdef SHUTDOWN
+#ifdef SHUTDOWN // シャットダウン信号を使う場合のピン設定，LOWは活動中，HIGHは処理終了を示す．
   pinMode(SHUTDOWN_PIN, OUTPUT);
   digitalWrite(SHUTDOWN_PIN, LOW);
 #endif /* SHUTDOWN */
@@ -140,31 +148,24 @@ void setup() {
   Serial.begin(9600);
 #endif /* DEBUG */
 #ifdef SERIAL_OUT
-  serialCom.begin(9600);
+  serialCom.begin(9600); // 処理結果を返信するためのシリアルポートの設定
 #endif /* SERIAL_OUT */
-#ifdef DEBUG
-  Serial.println("start!");
-#ifdef SERIAL_OUT
-  serialCom.println("start!");
-#endif /* SERIAL_OUT */
-#endif /* DEBUG */
 #ifdef USE_RTC_8564NB
   int ans;
-  ans = skRTC.begin(PIN_NUMBER, INT_NUMBER, NULL, 12, 1, 10, 2, 15, 30, 0) ; // 2012/01/10 火 15:30:00 でRTCを初期化するが，すでに時刻を設定済みの場合，元々設定していた値が活かされる．
+  ans = skRTC.begin(RTC_PIN_NUMBER,INT_NUMBER,NULL,12,1,10,2,15,30,0) ;  // 2012/01/10 火 15:30:00 でRTCを初期化するが，すでに時刻を設定済みの場合，元々設定していた値が活かされる．
   if (ans != 0) {
 #ifdef DEBUG
     Serial.print("Failed initialization of the RTC ans=") ; // 初期化失敗
     Serial.println(ans) ;
-#ifdef SERIAL_OUT
-    serialCom.println("Failed initialization of the RTC");
-#endif /* SERIAL_OUT */
 #endif /* DEBUG */
+    //serialCom.println("Failed initialization of the RTC");
+    digitalWrite(RTC_LED_PIN,HIGH);
     delay(MAX_SLEEP);
 #ifndef DEBUG
   }
 #else /* DEBUG */
   } else {
-    Serial.println(F("Successful initialization of the RTC")) ;// 初期化成功
+     Serial.println("Successful initialization of the RTC") ;// 初期化成功
   }
 #endif /* DEBUG */
 #endif /* USE_RTC_8564NB */
@@ -172,81 +173,69 @@ void setup() {
   if (!sd.begin(SD_CHIP_SELECT, SDCARD_SPEED)) {
 #ifdef DEBUG
     Serial.println("Failed initialization of SD card") ; // 初期化失敗
-
-#ifdef SERIAL_OUT
-    serialCom.println("Failed initialization of SD card");
-#endif /* SERIAL_OUT */
 #endif /* DEBUG */
+    //serialCom.println("Failed initialization of SD card");
+    digitalWrite(SD_LED_PIN,HIGH);
     delay(MAX_SLEEP);
     sd.initErrorHalt();
   }
   if (!file.open(LOG_FILE_NAME, FILE_WRITE)) {
 #ifdef DEBUG
     Serial.println("file.open error");
-#ifdef SERIAL_OUT
-    serialCom.println("file.open error");
-#endif /* SERIAL_OUT */
 #endif /* DEBUG */
+    //serialCom.println("file.open error");
     delay(MAX_SLEEP);
   }
   file.flush();
 #endif /* USE_SD */
-#ifdef STARTUP_LED
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
-  delay(INTERVAL);
-  digitalWrite(ledPin, LOW);
-#endif /*  STARTUP_LED */
+#ifdef DEBUG
+  Serial.println("start!");
+#endif /* DEBUG */
 }
-
+/*******************************************************************************
+*  処理終了通知をピンで実施する場合
+*******************************************************************************/
 #ifdef SHUTDOWN
-void shutdown() {
+void shutdown(){
   digitalWrite(SHUTDOWN_PIN, HIGH);
 }
 #endif /* SHUTDOWN */
-
-void doWork() {
+/*******************************************************************************
+*  実際の処理
+*******************************************************************************/
+void doWork(){
+  char buff[BUFF_MAX];
+  char sensorVal[BUFF_MAX];
   getSensorValue(sensorVal);
 #ifdef USE_RTC_8564NB
   byte tm[7];
-  char *buff;
+  char tbuff[TIME_BUFF_MAX];
   skRTC.rTime(tm);
-  skRTC.cTime(tm, (byte *)tbuff);
-  buff = tbuff + strlen(tbuff);
-  //sprintf(buff,"%s : %s",tbuff,sensorVal);
-  sprintf(buff, " : %s", sensorVal);
-  //sprintf(buff,"%s : Hello World!",tbuff);
+  skRTC.cTime(tm,(byte *)tbuff);
+  sprintf(buff,"%s : %s",tbuff,sensorVal);
 #else /* USE_RTC_8564NB */
-  sprintf(buff, "%s", sensorVal);
-  //sprintf(buff,"Hello World!");
+  sprintf(buff,"unkown : %s",sensorVal);
 #endif /* USE_RTC_8564NB */
-  buff=tbuff;
 #ifdef USE_SD
   file.println(buff);
   file.flush();
 #endif /* USE_SD */
 #ifdef SERIAL_OUT
-  for (int i = 0; i < 5; i++) {
-    serialCom.println(buff);
-    Serial.println(buff);
+  serialCom.println(buff);
 #ifdef DEBUG
-    Serial.println(buff);
-#endif /* DEBUG */
-    delay(1000);
-  }
-  delay(MAX_SLEEP);
-#else /* SERIAL_OUT */
   Serial.println(buff);
-  //delay(30000);
-  delay(MAX_SLEEP);
+#endif /* DEBUG */
 #endif /* SERIAL_OUT */
 }
-
-void loop() {
+/*******************************************************************************
+*  ループ
+*******************************************************************************/
+void loop(){
   // read the state of the pushbutton value:
   doWork();
 #ifdef SHUTDOWN
   shutdown();
+#else /* SHUTDOWN */
+  delay(30000);
 #endif /* SHUTDOWN */
 }
